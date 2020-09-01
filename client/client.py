cf25649d1ad74859aa3e9f320110fba0b69717e7
Latsis <lateh07@gmail.com>
2020-09-01 10:50:13 +0300

    added some client functionality, still WIP

--- client/client.py
+++ client/client.py
@@ -1,37 +1,53 @@
-# get list of events with GET
-# curl -i -X GET http://localhost:5000/event
-
-# create event with POST
-# curl -i -X POST -H 'Content-Type: application/json' --data @<json_filename>.json http://localhost:5000/event
-
-# Modify event with PUT
-# curl -i -X PUT -H 'Content-Type: application/json' -H 'Authorization: Basic <creator_token>' --data @<json_filename>.json http://localhost:5000/event/<event_id>
-
-# Get event attendee list with GET
-# curl -i -X GET -H 'Authorization: Basic <creator_token>' http://localhost:5000/event/<event_id>/attendees
-
-# Create event attendee with POST
-# curl -i -X POST -H 'Content-Type: application/json' -H 'Authorization: Basic <creator_token>' --data @<json_filename>.json http://localhost:5000/event/<event_id>
-
-# Update event image with PUT
-# curl -i -X POST -H "Content-Type: application/json" -H 'Authorization: Basic <creator_token>' --data '{"image": "/dev/null"}' localhost:5000/event/<event_id>/image
-
-# Delete event image with DELETE
-# curl -i -X DELETE -H 'Authorization: Basic <creator_token>' localhost:5000/event/<event_id>/image
-
-# Example json:
-# {
-#     "title":"eventti",
-#     "time":"2020-02-02T00:00:00+0200",
-#     "location":"Tellus",
-#     "creator_name":"sakkoja",
-#     "description":"this is an event",
-#     "image":"http://google.com"
-# }
-
 import json, datetime, requests, string, re
 
 API_URL = "http://127.0.0.1:5000"
+
+#testing
+TEST_EVENT_ID = "WUQFSS9V"
+TEST_CREATOR_TOKEN = "ORDCXHZNMOZN7FKC8503XI1RQ2RQTCGHDO9MGFSQLD2MV0WYVJHNGGK639P9T89I"
+
+
+def menu():
+    """Menu loop""" #Courtesy of https://stackoverflow.com/questions/34192588/simple-menu-in-python-3
+    choice ='0'
+    print("Welcome to use Notikums, the ultimate event management platform!")
+    print("--------------------------------")
+    while True:
+        print("Select 1 to get list of all events")
+        print("Select 2 to get information about a specific event")
+        print("Select 3 to create a new event")
+        print("Select 4 to modify or delete event")
+        print("Select 5 to list event attendees")
+        print("Select 6 to join event")
+        print("Select 7 to modify participation info or leave event")
+        print("Select 8 to get information about a specific attendee")
+        print("Select Q to quit program")
+        print("--------------------------------")
+
+        choice = input ("What do you want to do? ")
+
+        if choice == "1":
+            get_events()
+        elif choice == "2":
+            get_single_event()
+        elif choice == "3":
+            create_event()
+        elif choice == "4":
+            modify_event()
+            #delete_event()
+        elif choice == "5":
+            get_event_attendees()
+        elif choice == "6":
+            create_attendee()
+        elif choice == "7":
+            modify_attendee()
+            #delete_attendee()
+        elif choice == "8":
+            get_attendee()
+        elif choice == "Q":
+            break
+        else:
+            print("Invalid choice.")
 
 
 def get_events():
@@ -46,22 +62,33 @@ def get_events():
         print("Creator: {}".format(event["creator_name"]))
         print("Description: {}".format(event["description"]))
         print("Image: {}".format(event["image"]))
-        print("--------------------")
+        print("--------------------------------")
     return
 
 
-def get_single_event(event_id):
+def get_single_event():
     """Get data for specific event"""
+
+    while True:
+        event_id = input("Enter event id: ")
+        if event_id:
+            break
+
     resp = requests.get(API_URL + "/event/{}".format(event_id))
-    event = resp.json()
-    print("Identifier: {}".format(event["identifier"]))
-    print("Title: {}".format(event["title"]))
-    print("Location: {}".format(event["location"]))
-    print("Time: {}".format(event["time"]))
-    print("Creator: {}".format(event["creator_name"]))
-    print("Description: {}".format(event["description"]))
-    print("Image: {}".format(event["image"]))
-    print("--------------------")
+    if resp.status_code == 200:
+        event = resp.json()
+        print("--------------------------------")
+        print("Identifier: {}".format(event["identifier"]))
+        print("Title: {}".format(event["title"]))
+        print("Location: {}".format(event["location"]))
+        print("Time: {}".format(event["time"]))
+        print("Creator: {}".format(event["creator_name"]))
+        print("Description: {}".format(event["description"]))
+        print("Image: {}".format(event["image"]))
+        print("--------------------------------")
+        return
+    print("Event does not exist!")
+    print("--------------------------------")
     return
 
 
@@ -101,23 +128,39 @@ def create_event():
     event_item["image"] = event_image
 
     resp = requests.post(API_URL + "/event", data=json.dumps(event_item), headers={"Content-type": "application/json"})
-    event = resp.json()
+    if resp.status_code == 201:
+        event = resp.json()
 
-    print("Identifier: {}".format(event["identifier"]))
-    print("Creator token: {}".format(event["creator_token"]))
-    print("Title: {}".format(event["title"]))
-    print("Location: {}".format(event["location"]))
-    print("Time: {}".format(event["time"]))
-    print("Creator: {}".format(event["creator_name"]))
-    print("Description: {}".format(event["description"]))
-    print("Image: {}".format(event["image"]))
-    print("--------------------")
+        print("--------------------------------")
+        print("Success! Event created")
+        print("Identifier: {}".format(event["identifier"]))
+        print("Creator token: {}".format(event["creator_token"]))
+        print("Title: {}".format(event["title"]))
+        print("Location: {}".format(event["location"]))
+        print("Time: {}".format(event["time"]))
+        print("Creator: {}".format(event["creator_name"]))
+        print("Description: {}".format(event["description"]))
+        print("Image: {}".format(event["image"]))
+        print("--------------------------------")
+        return
+    print("Something went wrong! Make sure the event information is given in the correct form.")
+    print("--------------------------------")
     return
 
 
-def modify_event(event_id, creator_token):
+def modify_event():
     """Modify previously created event"""
     event_item = {}
+
+    while True:
+        event_id = input("Enter event id: ")
+        if event_id:
+            break
+
+    while True:
+        creator_token = input("Enter your creator token: ")
+        if creator_token:
+            break
 
     event_title = input("Title: ")
     event_item["title"] = event_title
@@ -132,59 +175,73 @@ def modify_event(event_id, creator_token):
     event_image = input("Image URL: ")
     event_item["image"] = event_image
 
-    resp = requests.put(API_URL + "/event", data=json.dumps(event_item), headers={"Content-type": "application/json", "Authorization": "Basic {}".format(creator_token)})
-    event = resp.json()
-
-    print("Identifier: {}".format(event["identifier"]))
-    print("Creator token: {}".format(event["creator_token"]))
-    print("Title: {}".format(event["title"]))
-    print("Location: {}".format(event["location"]))
-    print("Time: {}".format(event["time"]))
-    print("Creator: {}".format(event["creator_name"]))
-    print("Description: {}".format(event["description"]))
-    print("Image: {}".format(event["image"]))
-    print("--------------------")
-    return
+    resp = requests.put(API_URL + "/event/{}".format(event_id), data=json.dumps(event_item), headers={"Content-type": "application/json", "Authorization": "Basic {}".format(creator_token)})
+    
+    if resp.status_code == 200:
+        event = resp.json()
+        print("--------------------------------")
+        print("Success! Event updated.")
+        print("Identifier: {}".format(event["identifier"]))
+        print("Creator token: {}".format(event["creator_token"]))
+        print("Title: {}".format(event["title"]))
+        print("Location: {}".format(event["location"]))
+        print("Time: {}".format(event["time"]))
+        print("Creator: {}".format(event["creator_name"]))
+        print("Description: {}".format(event["description"]))
+        print("Image: {}".format(event["image"]))
+        print("--------------------------------")
+        return
+    elif resp.status_code == 404:
+        print("--------------------------------")
+        print("Event not found")
+        print("--------------------------------")
+    elif resp.status_code == 401:
+        print("--------------------------------")
+        print("Unauthorized")
+        print("--------------------------------")
+    elif resp.status_code == 415: #TODO: returns if only some info is given
+        print("--------------------------------")
+        print("")
+        print("--------------------------------")
 
 
 def delete_event(event_id, creator_token):
     """Delete previously created event"""
-    pass
+    print("not implemented (yet)")
 
-def get_event_attendees(event_id, creator_token):
+def get_event_attendees():
     """Get attendee list for specific event"""
-    pass
+    print("not implemented (yet)")
 
-def get_attendee(event_id, user_token, creator_token):
+def get_attendee():
     """Get specific attendee for specific event"""
-    pass
+    print("not implemented (yet)")
 
-def create_attendee(event_id):
+def create_attendee():
     """Join event"""
-    pass
+    print("not implemented (yet)")
 
-def modify_attendee(event_id, user_token, creator_token):
+def modify_attendee():
     """Modify prevoiusly created attending information for specific user in specific event"""
-    pass
+    print("not implemented (yet)")
 
-def delete_attendee(event_id, user_token, creator_token):
+def delete_attendee():
     """Delete previously created attending information for specific user in specific event"""
-    pass
+    print("not implemented (yet)")
 
 def get_event_image(event_id):
     """Get image for single event by event_id"""
     resp = requests.get(API_URL + "/event/{}/image".format(event_id))
     event = resp.json()
     print("Image: " + event["image"])
-    return
 
 def add_event_image(event_id, creator_token):
     """Add or change image for specific event"""
-    pass
+    print("not implemented (yet)")
 
 def delete_event_image(event_id, creator_token):
     """Delete image from specific event"""
-    pass
+    print("not implemented (yet)")
 
 def get_event_description(event_id):
     """Get description for single event by event_id"""
@@ -210,6 +267,21 @@ def get_event_time(event_id):
     return
 
 
+menu()
+
+
+
+
+#create_event()
+
+#get_event_description(TEST_EVENT_ID)
+
+#get_event_image(TEST_EVENT_ID)
+
+#get_event_location(TEST_EVENT_ID)
+
+#get_event_time(TEST_EVENT_ID)
+
 
 # TEST EVENT RESPONSE JSON
 # {
@@ -222,25 +294,3 @@ def get_event_time(event_id):
 #     'description': 'DESCRIPTION2',
 #     'image': 'URL2'
 # }
-
-#testing
-TEST_EVENT_ID = "WUQFSS9V"
-TEST_CREATOR_TOKEN = "ORDCXHZNMOZN7FKC8503XI1RQ2RQTCGHDO9MGFSQLD2MV0WYVJHNGGK639P9T89I"
-
-get_events()
-
-get_single_event(TEST_EVENT_ID)
-
-#create_event()
-
-modify_event(TEST_EVENT_ID, TEST_CREATOR_TOKEN)
-
-get_event_attendees(TEST_EVENT_ID, TEST_CREATOR_TOKEN)
-
-get_event_description(TEST_EVENT_ID)
-
-get_event_image(TEST_EVENT_ID)
-
-get_event_location(TEST_EVENT_ID)
-
-get_event_time(TEST_EVENT_ID)
